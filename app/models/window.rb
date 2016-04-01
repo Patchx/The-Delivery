e3ed9e16671f70654cad840e9d7ead3144fb7e33
Patchx <robert.anderson.fl@gmail.com>
2016-04-01 13:36:12 -0400

    removed gosu gem and code

--- app/models/window.rb
+++ app/models/window.rb
@@ -1,12 +1,3 @@
-class Window < Gosu::Window
-  def initialize
-    super(640, 480, false)
-    self.caption = "Let's play a game!"
- 
-    @background_image = Gosu::Image.new(self, "public/Space.png", true)
-  end
- 
-  def draw
-    @background_image.draw(0,0,0)
-  end
+class Window
 end
+
